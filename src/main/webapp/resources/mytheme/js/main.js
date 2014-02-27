52a87e4fccd61321ec03bc1faeb56d63ca70eaad
guanyuan <yuanguan89@gmail.com>
2014-02-27 23:47:23 +0800

    main.js contain the logic of game but with syntax problem

--- src/main/webapp/resources/mytheme/js/main.js
+++ src/main/webapp/resources/mytheme/js/main.js
@@ -1,14 +1,68 @@
-jQuery(document).ready(function($) {
+$(document).ready(function(){
 
-	$('#msg').html("This is updated by jQuery")
+    var imgPrefix = '<img src="<c:url value="/resources/img/';
+    var imgSuffix = '" />" style="width: 100px; margin-left: 10px;"/>';
 
-});
+    var cardPointArray = ${shuffledCardsPoints};
+    var cardNameArray = ${shuffledCardsNames};
 
-var imgIndex = 4;
-var imgPrefix = '<img src="<c:url value="/resources/img/';
-var imgSuffix = '" />" style="width: 100px; margin-left: 10px;"/>';
+    var index = 0;
+    var playerPoints = 0;
+    var hostPoints = 0;
 
-function appendText = getImageSrc(){
-imgIndex++;
-appendText = imgPrefix + $A[imgIndex] + imgSuffix;
-}
+      $("#playerStart").click(function(){
+          $(".backCards").show();
+          playerPoints += (cardPointArray.get(0)) + cardPointArray.get(1));
+          hostPoints +=  (cardPointArray.get(2)) + cardPointArray.get(3));
+          index += 4;
+          $("#playerScore").text(playerPoints);
+          $("#hostScore").text(hostPoints);
+      });
+
+      $("#playerHit").click(function(){
+            var imgSrc = imgPrefix + cardNameArray.get(index) + imgSuffix;
+            index++;
+            $("#playerCards").append(imgSrc);
+            playerPoints += cardPointArray.get(index);
+            $("#playerScore").text(playerPoints);
+      });
+
+      $("#hostHit").click(function(){
+            var imgSrc = imgPrefix + cardNameArray.get(index) + imgSuffix;
+            index++;
+            $("#hostCards").append(imgSrc);
+            hostPoints += cardPointArray.get(index);
+            $("#hostScore").text(hostPoints);
+            });
+
+      $("#playerStand").click(function(){
+         $("#playerHit").attr("disabled", true);
+      });
+
+      $("#hostStand").click(function(){
+        $("#hostHit").attr("disabled", true);
+      });
+
+      $("#playerScore").change(function(){
+            var score = $("#playerScore").val();
+            if(score > 21) {
+                $("#hostScore").html("Host Wins");
+                $("#hostScore").css("color","red");
+                $("#replayDiv").html(<button id="replayBtn" type="button"> Replay</button>);
+            }
+      });
+
+      $("#hostScore").change(function(){
+            var score = $("#hostScore").val();
+            if(score > 21){}
+                $("#playScore").text("Player Wins");
+                $("#playScore").css("color","red");
+                $("#replayDiv").html("<button id="replayBtn" type="button"> Replay</button>");
+            }
+
+      });
+
+      $("#replayBtn").click(function(){
+             location.reload();
+      });
+    });
